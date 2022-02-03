028c7198c48c3263a99fc909a6ff400cff2d304f
lujohnny95 <lu_johnny95@yahoo.com>
2022-02-03 18:14:32 +0000

    Search form implemented

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
 import useFetchJobs from "./useFetchJobs";
 import { Container } from "react-bootstrap";
-import Job from "./Job.js";
+import Job from "./Job";
 import JobsPagination from "./JobsPagination";
+import SearchForm from "./SearchForm";
 
 function App() {
   const [params, setParams] = useState({})
   const [page, setPage] = useState(1)
   const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
 
+  function handleParamChange(e) {
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams(prevParams => {
+      return { ...prevParams, [param]: value }
+    })
+  }
+
   return (
     <Container className="my-4">
       <h1 className="mb-4">Job-Stack</h1>
+      <SearchForm params={params} onParamChange={handleParamChange} />
       <JobsPagination page={page} setPage={setPage} hasNextPage={true}/>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try refreshing!</h1>}
